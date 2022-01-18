6d0604f4d559fc9f811b9dba045cf5b485c2b4a4
cesarsiguencia <cesar.siguencia@gmail.com>
2022-01-18 18:26:25 -0500

    writing generic fetch function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,28 @@
-var getUserRepos = function() {
-    console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+// var getUserRepos = function() {
+//     console.log("function was called");
+//     fetch("https://api.github.com/users/octocat/repos").then(function(response){
+//         response.json().then(function(data){
+//             console.log(data);
+//         })
+
+//     });
+    
+//   };
   
 
-getUserRepos();
+// getUserRepos();
+
+
+var getUserRepos = function(octocat) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + octocat + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
+  getUserRepos("octocat");
